703437dc1f006f8834bf453510593ffaca80f0be
Rachel <hurui.lcy@gmail.com>
2025-02-05 20:13:24 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Disney Characters Showcase</h1>
-      
       <div>
         <DisneyFetch />
       </div>
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 })
